508f7ae9c42e409c70768460e8aaccb8af47e932
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-05-11 15:49:26 +0900

    백준 알고리즘 1316번 그룹 단어 체커 앞뒤단어 체크 하기, 이중포문에서 두번째 포문 range 범위

---
+++ 문자열/1316.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt','r')
+
+n = int(input())
+result = n                          # 처음 결과 갯수는 처음 받은 단어갯수와 동일하게 선언
+for i in range(n):
+    word = input()
+    for j in range(0,len(word)-1):
+        if word[j]==word[j+1]:              #앞뒤 단어 같으면
+            pass                            #패스
+        elif word[j] in word[j+1:]:         # 먼저 나온 단어가 뒤에 또 나오는지 확인
+            result -= 1                     # 있으면 원래 단어 갯수 -1
+            break                           # 포문 탈출
+print(result)
