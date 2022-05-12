fa3667e86e047e1eadc3d064a6f6f8f052600c02
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-05-12 18:07:35 +0900

    백준 알고리즘 1193번

---
+++ 기본수학1/1193.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt','r')
+
+n = int(input())
+line = 1
+
+while n > line:             # 입력받은 수가 몇번째 라인에 있는지 확인
+    n -= line
+    line += 1
+print(n,line)
+if line%2==0:               # 라인이 짝수라면
+    a = n
+    b = line-n+1
+else:                       # 라인이 홀수라면
+    a=line-n+1
+    b=n
+print(a,'/',b,sep='')
+
+
