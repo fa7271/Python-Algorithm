08163e0ecf61a63163976f14721ee8faea255b19
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-05-13 18:20:04 +0900

    백준 알고리즘 10250

---
+++ 기본수학1/10250.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt','r')
+
+T = int(input())
+# H 층수 W = 방수 N = 손님
+for i in range(T):
+    H,W,N = list(map(int,input().split()))
+    num = (N//H +1)                          #라인 = (N//H)+1 라인
+    line = N%H                               # % 나머지반환
+    if line == 0:                             #층이 H의 배수이면
+        num = N//H
+        line = H
+    print(line*100+num)
+
