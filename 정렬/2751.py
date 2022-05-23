ab9029bed137e8c978c663cd031ecad25d4f2611
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-05-23 18:39:04 +0900

    백준 알고리즘 2751 수 정렬하기2 조인함수 vs for문 사용

--- 정렬/2751.py
+++ 정렬/2751.py
@@ -9,10 +9,10 @@ for i in range(N):
     array.append(int(sys.stdin.readline()))
 
 new = sorted(array)
-
-for i in new:
-    print(i)
-
-
+sys.stdout.write('\n'.join(map(str,new)))
+# for i in new:
+#     print(i)
 
 
+# 조인 함수사용시  164440 kb / 1236 ms
+# for 문 사용시   83488 kb / 1480 ms
