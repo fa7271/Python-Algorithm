42dc07905b73aeb1e81053c6741f967d9e72ed33
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-05-24 19:14:21 +0900

    백준 알고리즘 10814 나이순 정렬
    나이기준 정렬

---
+++ 정렬/10814.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt','r')
+
+# input = sys.stdin.readline
+# input = int(sys.stdin.readline())
+
+
+N = int(sys.stdin.readline())
+array = []
+for i in range(N):
+    array.append(list(sys.stdin.readline().split()))
+
+
+array.sort(key = lambda x:int(x[0]))
+for i in array:
+    print(i[0],i[1])
+
+
