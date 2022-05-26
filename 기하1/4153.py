e5d09f71a97e0a3cc5783515e824582d63b2759c
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-05-26 22:48:50 +0900

    백준 알고리즘 4153 직각삼각형

---
+++ 기하1/4153.py
@@ -0,0 +1,38 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt','r')
+
+# input = sys.stdin.readline
+# input = int(sys.stdin.readline())
+#
+# list_x = []
+# list_y = []
+# list_z = []
+# for i in range(3):
+#     x,y,z = map(int,input().split())
+#     list_x.append(x**2)
+#     list_y.append(y**2)
+#     list_z.append(z**2)
+#
+# for i in range(len(sys.stdin.readline())-1):
+#     if list_z[i] == list_x[i] + list_y[i]:
+#         print('right')
+#     else:
+#         print('wrong')
+#
+# print(0,0,0,sep= " ")
+
+
+li = []
+
+while True:
+    a = list(map(int,input().split()))
+    if(a == [0, 0, 0]):
+        break
+    li.append(a)
+
+for i in li:
+    i.sort()
+    if(i[0]**2 + i[1]**2 == i[2]**2):
+        print("right")
+    else:
+        print("wrong")
