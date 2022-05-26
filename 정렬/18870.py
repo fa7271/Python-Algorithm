6e86b68ba847e18b62f2d05268a395da460a8681
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-05-26 14:00:48 +0900

    백준 알고리즘 10815 숫자 카드
    딕셔너리와 언팩킹 한번 더 공부

---
+++ 정렬/18870.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(input())
+arr = list(map(int, input().split()))
+arr_list = sorted(list(set(arr)))       # 중복제거
+arr_dic = {}
+
+for i in range(len(arr_list)):          # arr_list 에 길이만큼 포문을 돌려
+    arr_dic[arr_list[i]] = i            # 딕셔너리를 만들어준다
+for i in range(n):                      # 입력받은 n 만큼 포문을 돌려
+    arr[i] = arr_dic[arr[i]]            # 딕셔너리에서 키에 arr[i]를 넣어 벨류를 찾아 arr를 만들어줌
+print(*arr)
+# * 는 언팩킹 [] 까줌
+# for i in range(n):
+#     print(arr[i], end = ' ')   print(*arr) 대신 가능
+
+
