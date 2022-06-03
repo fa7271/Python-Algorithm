da143719d31924a7c178545f06414cae170e341a
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-06-03 15:20:04 +0900

    백준 알고리즘 15649번 N과 M (1) 다른 방법으로의 백트래킹

--- 백트래킹/15649.py
+++ 백트래킹/15649.py
@@ -1,21 +1,41 @@
 import sys
 sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 
-input = sys.stdin.readline
-# input = int(sys.stdin.readline())
+# input = sys.stdin.readline
+# # input = int(sys.stdin.readline())
+#
+# n, m = map(int, input().split())
+# s = []
+#
+# def f():
+#     if len(s) == m:
+#         print(*s)
+#         return
+#
+#     for i in range(1, n + 1):
+#         if i in s:
+#             continue
+#         s.append(i)
+#         f()
+#         s.pop()
+# f()
 
-n, m = map(int, input().split())
-s = []
 
-def f():
-    if len(s) == m:
-        print(*s)
+def solve(d, n, m):
+    if d == m:
+        print(" ".join(map(str, result)))
         return
+    for i in range(n):
+        if visited[i] == False:
+            visited[i] = True
+            result.append(i + 1)
+            solve(d + 1, n, m)
+            visited[i] = False
+            result.pop()
 
-    for i in range(1, n + 1):
-        if i in s:
-            continue
-        s.append(i)
-        f()
-        s.pop()
-f()
+
+n, m = map(int, sys.stdin.readline().split())
+
+visited = [False] * n
+result = []
+solve(0, n, m)
