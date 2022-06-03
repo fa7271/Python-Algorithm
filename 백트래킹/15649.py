473776b3609e5c5c8ce6d65636d5cc18329ad8cb
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-06-03 14:19:23 +0900

    백준 알고리즘 15649번 N과 M (1)

--- 백트래킹/15649.py
+++ 백트래킹/15649.py
@@ -4,16 +4,18 @@ sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 input = sys.stdin.readline
 # input = int(sys.stdin.readline())
 
-def factorial(n):
-    num = 1
-    for i in range(1, n+1):
-        num *= i
-    return num
+n, m = map(int, input().split())
+s = []
 
+def f():
+    if len(s) == m:
+        print(*s)
+        return
 
-T = int(input())
-
-for _ in range(T):
-    n, m = map(int, input().split())
-    res = factorial(m) // (factorial(n) * factorial(m - n))
-    print(res)
+    for i in range(1, n + 1):
+        if i in s:
+            continue
+        s.append(i)
+        f()
+        s.pop()
+f()
