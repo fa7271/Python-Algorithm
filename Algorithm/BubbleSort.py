933c13a237c547e4ca89662aba9dadb8b555955e
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-06-06 14:34:50 +0900

    BubbleSort

--- Algorithm/BubbleSort.py
+++ Algorithm/BubbleSort.py
@@ -3,12 +3,10 @@ sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 
 
 # 버블 정렬의 범용성을 높이기 위해 함수로 만듬
-def bubbleSort(arr):
-    n = len(arr) # 배열의 크기를 측정
+def bubble_Sort(arr):
+    n = len(arr) # 배열의 크기
 
-    # 배열의 크기만큼 반복
     for i in range(n):
-
         # 배열의 총 크기에서 i의 값과 1을 뺀 만큼 반복
         for j in range(0, n - i - 1):
 
@@ -16,10 +14,9 @@ def bubbleSort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j] # 서로 위치를 변환
 
-# 배열 예시
-arr = [64, 34, 25, 12, 57, 93, 1, 123]
+arr = [10,20,40,30,22,64,32,77,24,29]
 
-bubbleSort(arr)
+bubble_Sort(arr)
 
 for i in range(len(arr)):
     print("%d " %arr[i],end = "")
