9986ad9c870448d800ada256995a1d9899b82d6f
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-06-07 23:16:45 +0900

    Insert 알고리즘

---
+++ Algorithm/Insert.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
+
+
+def insertjon_sort(arr):
+    for i in range(1, len(arr)):
+        for j in range(i, 0, -1):
+            if arr[j - 1] > arr[j]:
+                arr[j - 1], arr[j] = arr[j], arr[j - 1]
+
+
