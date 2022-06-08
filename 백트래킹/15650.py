bb8d43bfdff1a395af31fdef47f10f92ae74e1c0
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-06-08 15:31:16 +0900

    백준 알고리즘 15650 N과 M (2) 백트래킹 중복숫자 제거 재귀함수/조합 두 방법

---
+++ 백트래킹/15650.py
@@ -0,0 +1,34 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
+
+
+# from itertools import combinations
+#
+# N, M = map(int, input().split())
+#
+# p = combinations(range(1, N+1), M)
+# for i in p:
+#     print(*i)
+
+
+n,m = map(int,input().split())
+visited = [False] * n
+result = []
+
+def solve(num):
+    if num == m:
+        print(*result)
+        return
+
+    for i in range(n):
+        if visited[i] == False:
+            visited[i] = True
+            result.append(i + 1)
+            solve(num+1)
+            visited[i] = True
+            result.pop()
+            for j in range(i+1, n):
+                visited[j] = False
+
+solve(0)
+
