d5532c4f81b691cde4a6b4fe519b4c0d21e9c755
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-06-10 18:02:06 +0900

    백준 알고리즘 15652 N과 M (4) 백트래킹 중복가능 오름차순

---
+++ 백트래킹/15652.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
+
+
+n,m = map(int,input().split())
+visited = [False] * n
+result = []
+
+def solve(num):
+    if num == m:
+        print(*result)
+        return
+
+    for i in range(n):
+        if visited[i] == False:
+            # visited[i] = True
+            result.append(i + 1)
+            solve(num+1)
+            visited[i] = True
+            result.pop()
+            for j in range(i+1, n):
+                visited[j] = False
+
+solve(0)
+
