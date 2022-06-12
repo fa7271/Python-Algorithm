7b06a71a9c9e815b2153d69aecb13ab7db50c803
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-06-12 17:47:12 +0900

    백준 알고리즘 15649번 N과M (1) 중복 제거

--- 백트래킹/15649.py
+++ 백트래킹/15649.py
@@ -20,31 +20,35 @@ sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 #         s.pop()
 # f()
 
-# n, m = map(int, sys.stdin.readline().split())
-#
-#
-# def solve(num):
-#     if num == m:
-#         print(" ".join(map(str, result)))
-#         return
-#     for i in range(n):
-#         if visited[i] == False:
-#             visited[i] = True
-#             result.append(i + 1)
-#             solve(num+1)
-#             visited[i] = False
-#             result.pop()
-#
-#
-# visited = [False] * n
-# result = []
-# solve(0)
+n, m = map(int, sys.stdin.readline().split())
+visited = [False] * (n+1)                   # 인덱스 1부터 바로 접근하기 위해서 >> 편리
+result = []
 
-from itertools import permutations
+def solve(num):
+    if num == m:                            # 탈출 조건 깊이와 m 이 같을떄
+        print(*result)
+        return
+    for i in range(1,n+1):
+        if visited[i] == False:             # 방문 하지 않은곳이라면
+            visited[i] = True               # 방문여부 체크해줌 방문했으면 True 로 바꿔줌
+            result.append(i)                # result 결과 리스트 값 추가
+            solve(num+1)                    # 다음꺼 재귀 호출
+            print(result)
+            visited[i] = False              # False 로 해주지 않으면 그 다음꺼 탐색 불가능
+            result.pop()                    # 마지막꺼 나오니까 pop으로 빼내줌
 
-N,M = map(int,input().split())
 
-P = permutations(range(1,N+1),M)
 
-for i in P:
-    print(" ". join(map(str,i)))
+solve(0)
+
+# from itertools import permutations
+#
+# N,M = map(int,input().split())
+#
+# P = permutations(range(1,N+1),M)
+#
+# for i in P:
+#     print(" ". join(map(str,i)))
+
+
+# 시간복잡도 중복이 가능하면 O(N^2) (10개 까지 가능)   중복불가 > O(N!) (8개까지 가능)
