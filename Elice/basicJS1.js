edd4a3ade87fb967fb879b744f711d48f1765a14
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-08-10 17:13:16 +0900

    220810 elice 프리트랙

--- Elice/basicJS1.js
+++ Elice/basicJS1.js
@@ -31,3 +31,29 @@ do{
 } while (조건){
     명령
 }
+
+주사위 게임
+var dice = Math.floor(Math.random() * 6) +1;
+// math.random 은 0 에서 1사이 임의 숫자 출력
+
+소수 찾기
+function prime(n){
+    var sosu = 2;
+    while (n > sosu){
+        if(n % sosu ===0){
+            return false;
+        }else {
+            sosu ++;
+        }
+    }return true;
+}
+
+믄자열 거꾸로 출력하기
+
+function reverse(str) {
+    var reverseStr = '';
+    for (var i = str.length - 1; i >= 0; i--) { // 반대로 하나씩 내려옴
+        reverseStr = reverseStr + str.charAt(i); //charat 란 특정 인덱스에 위치하는 유니코드 단일 문자를 반환
+    }return reverseStr
+}
+console.log(reverse('hello'))
---
+++ Elice/example/code.js
@@ -0,0 +1,17 @@
+function timesTable(n) {
+    if(n == 2){
+        for(var i = 1; i<10; i++){
+            document.write("2 " + "x" + i + "=" + i*2);
+            document.write('<br>');
+        }
+    }
+    if(n == 3){
+        for(var i = 1; i<10; i++){
+            document.write("2 " + "x" + i + "=" + i*2);
+            document.write('<br>');
+        }
+    }
+}
+timesTable(2);
+timesTable(3);
+
