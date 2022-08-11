ec01fc4176902eaba42464d5cd22781212a0916a
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-08-11 20:20:32 +0900

    220811 elice 프리트랙 초급 클리어

--- Elice/example/code2.js
+++ Elice/example/code2.js
@@ -5,3 +5,32 @@
 //         console.log(i +" x "+ j + " = " + i * j)
 //     }
 // }
+
+function solution(num) {
+    var answer = [];
+
+    for(var i=2; i<num; i++) {
+        if(num % i == 0) {
+            answer.push(i);
+        }
+    }
+    var res = [];
+    for (var i = 0; i <answer.length; i++) {
+        if (answer[i] == 2) {
+            res.push(2 + "의 배수입니다.");
+        }else if( answer[i] == 3){
+            res.push(3 + "의 배수입니다.");
+        }else if( answer[i] == 5){
+            res.push(5 + "의 배수입니다.");
+        }else if( answer[i] == 7){
+            res.push(7 + "의 배수입니다.");
+        }
+    }
+    var arr = res.join("\n");
+    console.log(arr);
+    return arr;
+}
+
+
+
+solution(21);
