0d96fd053d1ffe764502173e743af624c472b413
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-08-23 21:19:18 +0900

    220823 백준알고리즘 3003 킹, 퀸, 룩, 비숍, 나이트, 폰

--- 사칙연산/3003.py
+++ 사칙연산/3003.py
@@ -1,1 +1,8 @@
-https://www.acmicpc.net/problem/3003
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt','r')
+
+arr = [1, 1, 2, 2, 2, 8]
+
+x = list(map(int,input().split()))
+for i in range(6):
+    print(arr[i] - x[i] , end=" ")
