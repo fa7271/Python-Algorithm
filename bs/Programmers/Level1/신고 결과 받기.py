04928bb7d34948190807495c4ec67e19156508ce
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-08-28 21:57:46 +0900

    220828 선고 결과 받기

--- bs/Programmers/Level1/신고 결과 받기.py
+++ bs/Programmers/Level1/신고 결과 받기.py
@@ -7,25 +7,59 @@ def solution(id_list, report, k):
         arr[id_list[i]] = set()
         answerDic[id_list[i]] = 0
 
+    # print(answerDic) {'muzi': 0, 'frodo': 0, 'apeach': 0, 'neo': 0}
+    # print(answerDic)
     for i in range(len(report)):
         reporter, reported = report[i].split(" ")
         arr[reported].add(reporter)
+    print(arr)
 
-    succ = [];
+    res = []
     for name, v in arr.items():
-        if(len(v) >= k):
-            succ.append(name)
+        res.append(len(arr.get(name)))
+    print(res)
 
-    for id, rpt  in arr.items:
-        reporter, reported = report[i].split(" ")
-        # if id in succ:
-        #     for i in range(len(list(rpt.values())))
+    result = {}
+    for i in range(len(res)):
+        result[id_list[i]] = res[i]
 
-        for j in range(len(succ)):
-            if(rpt):
-                answerDic[reporter] += 1
+    print(result)
 
-    answer = list(answerDic.values())
+    value = list(result.keys())
+
+    sol = {}
+    for i in range(len(id_list)):
+        sol[id_list[i]] = 0
+
+    # print(sol)
+    x = []
+    for i in range(len(result)):
+        if(result.get(value[i]) >= k):
+            print(arr[value[i]])
+
+            x.append((arr[value[i]]))
+
+    print("----------")
+    print(x[0])
+    print(x[1])
+
+    # for value in result.values():
+    #     list.append(value, end=',')
+    #     print(list)
+
+
+
+    # print(result)
+    # for id, rpt  in arr.items:
+    #     reporter, reported = report[i].split(" ")
+    #     # if id in succ:
+    #     #     for i in range(len(list(rpt.values())))
+    #
+    #     for j in range(len(succ)):
+    #         if(rpt):
+    #             answerDic[reporter] += 1
+    #
+    # answer = list(answerDic.values())
 
     #1. 신고 처리된 사람(k번 이상 신고 받은 사람)
     #1-1 신고 횟수 계산
@@ -38,4 +72,13 @@ def solution(id_list, report, k):
     return answer
 
 solution(["muzi", "frodo", "apeach", "neo"],["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],2)
-//체크
+# solution(["con", "ryan"],["ryan con", "ryan con", "ryan con", "ryan con"],3)
+
+# 1. 한 사람당 한 명을 신고할 수 있다.
+# 1-1 한 사람 똑같은 사람을 신고 할 수 없다 >> 신고 당한사람이 중복하여 들어오면 set 으로 한개로 만들어줌
+# 1-2 키는 신고당한 아이디 / 벨류는 신고한 사람의 아이디
+#
+# 2. 신고 당한 사람이(report) 의 총 카운트가 k 보다 높으면 신고 한 사람들 의 카운트가 올라간다.
+# arr의 딕셔너리를 돌면서
+
+
