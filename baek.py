683fd96f5d6d289d5f2f4e6f5dd61d6216f76eaa
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-09-07 15:22:43 +0900

    220907 프로그래머스 소수만들기 combinatons (수열) import itertools

--- baek.py
+++ baek.py
@@ -3,3 +3,5 @@
 # return [sorted(array[a-1:b])[c-1] for a,b,c in commands]
 
 # 이중 리스트 [ [] ] 일때 [] 하나 빼고 출력하기 print(*) * 하고 출력하면됨
+
+# combination = list(itertools.combinations(nums, 3)) # 리스트 num 에서 3개 뽑느 수열
