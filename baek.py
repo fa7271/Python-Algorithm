29b912ce118671ec80f7069ac22df917c6e18516
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-09-09 15:23:56 +0900

    220909 리스트 '' 빼고 출력 방법

--- baek.py
+++ baek.py
@@ -5,3 +5,10 @@
 # 이중 리스트 [ [] ] 일때 [] 하나 빼고 출력하기 print(*) * 하고 출력하면됨
 
 # combination = list(itertools.combinations(nums, 3)) # 리스트 num 에서 3개 뽑느 수열
+
+#''.join(리스트)''.join(리스트)를 이용하면 매개변수로 들어온 ['a', 'b', 'c'] 이런 식의 리스트를 'abc'의 문자열로 합쳐서 반환해주는 함수인 것입니다.
+# ex
+# def solution(n):
+#     return int("".join(list(reversed(sorted(str(n))))))
+# print(solution(118372))
+
