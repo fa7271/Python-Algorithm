579299b2400e3c96d8f2e21635d55185c7ef3944
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-09-27 10:20:09 +0900

    220927 프로그래머스 뉴스 클러스트링

--- bs/Programmers/Level2/kakao_뉴스 클러스터링.py
+++ bs/Programmers/Level2/kakao_뉴스 클러스터링.py
@@ -1,19 +1,22 @@
-import re
+from collections import Counter
 
 
 def solution(str1, str2):
-    str1 = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]','', str1).lower()
-    str2 = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]','', str2).lower()
-    lst1=[]
-    lst2=[]
-    for x,y in zip(str1,str2):
-        lst1.append(x)
-        lst2.append(y)
-    result1 =[]
-    result2 =[]
-    for i in range(len(str1)-1):
-        result1.append(lst1[i:i+2])
-    for i in range(len(str2)-1):
-        result2.append(lst2[i:i+2])
-    print(set(result1) & set(result2))
-print(solution("FRANCE", "french+"))
+
+    result1 = [str1[i:i + 2].upper() for i in range(len(str1) - 1) if str1[i : i+2].isalpha()]
+    result2 = [str2[i:i + 2].upper() for i in range(len(str2) - 1) if str2[i : i+2].isalpha()]
+
+    if len(result1) == 0 and len(result2) == 0 : return 65536
+
+    A = Counter(result1)
+    B = Counter(result2)
+
+    sum_AB = sum((A | B).values())
+    mid_AB = sum((A & B).values())
+
+    return int(mid_AB / sum_AB * 65536)
+
+print(solution(	"1, 1, 2, 2, 3", "1,2,2,4,5}"))
+
+
+
