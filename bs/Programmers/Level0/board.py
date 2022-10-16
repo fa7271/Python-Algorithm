4c26155c187edb653f46f4ea3a462c475fc911e0
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-10-16 23:04:49 +0900

    221016 prgrammers id_pw

--- bs/Programmers/Level0/board.py
+++ bs/Programmers/Level0/board.py
@@ -1,12 +1,29 @@
+from itertools import combinations
+import re
 
 
-def solution(numbers, k):
-    res = []
-    if len(numbers) < 2 * k:
-        x,y = divmod((2*k),len(numbers))
-        res.append(numbers * x + numbers[:y] )
-    res = sum(res,[])
-    return res[2*k-2]
+def solution(id_pw, db):
 
-print(solution([1, 2, 3],3))
+    if id_pw[0].islower() == False :
+        return False
+    if id_pw[0] in sum(db,[]):
+        for i in range(len(db)):
+            if id_pw[1] == db[i][1]:
+                return "login"
+        else: return "wrong pw"
+    else:
+        return "fail"
+
+def solution(id_pw, db):
+    for i in db:
+        # id 여부 확인
+        if id_pw[0] in i:
+            if id_pw[1] == i[1]:
+                return "login"
+            else:
+                return "wrong pw"
+    return "fail"
+# print(solution(["p", "o", "s"],["sod", "eocd", "qixm", "adio", "soo"]))
+print(solution(["meosseugi", "1234"], [["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]]))
+# print(solution("apple","elppa"))
 # print(solution(	1, 13, 1))
