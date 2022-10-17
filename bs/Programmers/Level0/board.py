3f7b7d365e4c3fcc376975a61eedd84c53caa48c
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-10-17 23:51:07 +0900

    221016 prgrammers fianl 0 lv

--- bs/Programmers/Level0/board.py
+++ bs/Programmers/Level0/board.py
@@ -3,18 +3,6 @@ import re
 
 
 def solution(id_pw, db):
-
-    if id_pw[0].islower() == False :
-        return False
-    if id_pw[0] in sum(db,[]):
-        for i in range(len(db)):
-            if id_pw[1] == db[i][1]:
-                return "login"
-        else: return "wrong pw"
-    else:
-        return "fail"
-
-def solution(id_pw, db):
     for i in db:
         # id 여부 확인
         if id_pw[0] in i:
