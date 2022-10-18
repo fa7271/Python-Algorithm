09ba12d1a238cabfb9ecfb31e31d25438a16a9fa
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-10-18 23:53:30 +0900

    221016 prgrammers 최빈값 0 lv

--- bs/Programmers/Level0/board.py
+++ bs/Programmers/Level0/board.py
@@ -1,15 +1,31 @@
-from itertools import combinations, permutations
+
+def solution(array):
+    dict = {x : 0 for x in array}
+    if len(array) == 1:
+        return array[0]
+    for i in array:
+        if i in dict:
+            dict[i] += 1
+    sorted_dict = sorted(dict.items(), key = lambda item: item[1], reverse = True)
+    return -1 if sorted_dict[0][1] == sorted_dict[1][1] else sorted_dict[0][0]
 
 
-def solution(score):
-    score_list = [sum(i) for i in score]
-    list = sorted([sum(i) for i in score], reverse=True)
-    print(list)
-    print(score_list)
-    res = [list.index(i) +1 for i in score_list]
 
-    return res
-print(solution(	[[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]]))
+from collections import Counter
+
+def solution(array):
+    a = Counter(array).most_common(2)
+    if len(a) == 1:
+        return a[0][0]
+    if a[0][1] == a[1][1]:
+        return -1
+    return a[0][0]
+
+    # return -1 if max(dict.values() >=2) else max(dict)
+# print(solution([1, 2, 3, 3, 3, 4]))
+print(solution([1,2,3,3,3,4,4]))
+print(solution([5]))
+
 # print(solution([[80, 70], [90, 50], [40, 70], [50, 80]]))
 
 
