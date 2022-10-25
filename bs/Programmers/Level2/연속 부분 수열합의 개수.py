eaf1b86eda1604849aa49318c91d653a52299af9
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-10-25 17:36:45 +0900

    221025 programmers 연속 부분 수열합의 개수

--- bs/Programmers/Level2/연속 부분 수열합의 개수.py
+++ bs/Programmers/Level2/연속 부분 수열합의 개수.py
@@ -13,10 +13,12 @@ def solution(elements):
     #     for j in range(i+1, len(elements) + 1):
     #         print(elements[i:j],elements[:i])
     #         elements_set.add(sum(elements[i:j]))
+    elements_set = set(i for i in elements)
     elements = 2 * elements
-    for i in range(2,(len(elements)//2) + 1):
-        print(i)
-
+    for i in range((len(elements)//2)):
+        for j in range(2,(len(elements)//2) + 1):
+            elements_set.add(sum(elements[i:i+j]))
+    return (len(elements_set))
 print(solution(	[7,9,1,1,4]))
 
 #
