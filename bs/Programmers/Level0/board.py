b52d12a8d22fad75b24fcf7d94a647ecfc686cb1
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-03 23:38:42 +0900

    221102 프로그래머스

--- bs/Programmers/Level0/board.py
+++ bs/Programmers/Level0/board.py
@@ -1,7 +1,32 @@
+def solution(s):
 
-def solution(dots):
-    return (max(dots)[0] - min(dots)[0])*(max(dots)[1] - min(dots)[1])
-    m =  (y2-y1) / (x2-x1)
+    res = []
+    split = 1
 
-m =  (y2-y1) / (x2-x1)
-print(solution([[1, 4], [9, 2], [3, 8], [10, 4]] ))
+    while split < len(s):
+        x= list(map(''.join, zip(*[iter(s)]*split)))
+        count = 1
+        result =''
+        tmp =''
+
+        for i in range(1,len(x)):
+            if x[i] == x[i-1]:
+                count += 1
+            else:
+                if count != 1:
+                    result += str(count) + x[i]
+                else:
+                    result += x[i-1]
+
+            print(result)
+        res.append(result)
+        if len(x) <= 1 :
+            break
+        split += 1
+
+    # print(res)
+# print(solution("aabbaccc"))
+# print(solution("ababcdcdababcdcd"))
+# print(solution("abcabcdede"))
+print(solution("abcabcabcabcdededededede"))
+
