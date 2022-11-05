6e11ccf711f6e90eb137b06bb443bcf2d3057683
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-05 20:21:59 +0900

    221105 프로그래머스N으로 표현

---
+++ bs/Programmers/Level3/N으로 표현.py
@@ -0,0 +1,26 @@
+def solution(N, number):
+    if number == N:
+        return 1
+
+    list =[set() for i in range(8)] # 최솟값이 8 이기 때문
+
+    for i in range(len(list)):
+        list[i].add(int(str(N)*(i+1)))
+
+    for i in range(1,8):
+        for j in range(i):
+            for op1 in list[j]:
+                for op2 in list[i-j-1]:
+                    list[i].add(op1+op2)
+                    list[i].add(op1-op2)
+                    list[i].add(op1*op2)
+                    if op2 != 0:
+                        list[i].add(op1//op2)
+        if number in list[i]:
+            answer = i+1
+            break
+    else:
+        answer = -1
+    return answer
+
+print(solution(5,12))
