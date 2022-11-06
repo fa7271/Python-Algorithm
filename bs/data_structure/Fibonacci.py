cffce07fca8b0798ad3345a849e4028f7fc04576
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-06 11:52:12 +0900

    221106 fibonachi

--- bs/data_structure/Fibonacci.py
+++ bs/data_structure/Fibonacci.py
@@ -11,11 +11,36 @@
 # solution(2)
 
 
-def solution(x):
+# def solution(x):
+#
+#     if x <=1:
+#         return x
+#     else:
+#         return solution(x-1) + solution(x-2)
+#
+# print(solution(2))
 
-    if x <=1:
-        return x
+
+# top-down 방식
+def fibo_dp(x):
+    fib_array = [0,1]
+    if x < len(fib_array):
+        return fib_array[x]
     else:
-        return solution(x-1) + solution(x-2)
+        fib = fibo_dp(x-1) + fibo_dp(x-2)
+        fib_array.append(fib)
+        return fib
 
-print(solution(2))
+print(fibo_dp(7))
+
+
+# bottom-up 방식
+def fibo_dp_up(n):
+    if n<=1 :
+        return n
+    fibo_array = [0,1]
+    for i in range(2, n+1):
+        top_down = fibo_array[i-1] + fibo_array[i-2]
+        fibo_array.append(top_down)
+    return fibo_array[n]
+print(fibo_dp_up(7))
