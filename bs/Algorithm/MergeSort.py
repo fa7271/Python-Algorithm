9e6c8066b96effb3e9948c0127391d2800a12c16
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-14 14:04:48 +0900

    221114 sort

--- bs/Algorithm/MergeSort.py
+++ bs/Algorithm/MergeSort.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open('/Python/h.txt', 'r')
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 
 def merge_sort(a):
     n = len(a)
@@ -10,8 +10,10 @@ def merge_sort(a):
     mid = n // 2  # 중간을 기준으로 두 그룹으로 나눔
     g1 = merge_sort(a[:mid]) # 재귀 호출로 첫 번째 그룹을 정렬  처음 부터 mid 까지
     g2 = merge_sort(a[mid:]) # 재귀 호출로 두 번째 그룹을 정렬  mid 부터 끝까지
+    print(g1,g2)
 # 두 그룹을 하나로 병합
     result = []
+    # print(g1,g2,result)
     while g1 and g2:  # 두 그룹에 모두 자료가 남아 있는 동안 반복
         if g1[0] < g2[0]: # 두 그룹의 맨 앞 자료 값을 비교
             result.append(g1.pop(0))
@@ -22,7 +24,10 @@ def merge_sort(a):
         result.append(g1.pop(0))
     while g2:
         result.append(g2.pop(0))
+    print('on',result)
     return result
 
-d = [6, 8, 3, 9, 10, 1, 2, 4, 7, 5]
+d = [6, 8, 3, 9, 10, 12,15,13]
 print(merge_sort(d))
+
+# baeck 24060 참고
