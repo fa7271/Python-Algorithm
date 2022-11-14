9fb61f9080a239a7058df07d8079391f137e63e6
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-15 00:10:50 +0900

    221114 2579 계단 오르기

--- bs/동적 계획법/2579.py
+++ bs/동적 계획법/2579.py
@@ -5,15 +5,17 @@ sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 N = int(input())
 
 dp = [0] * 301
-arr = [0]*301
-for i in range(N):
-    arr[i] = int(input())
-
-dp[0] = arr[0]
-dp[1] = arr[0] + arr[1]
-dp[2] = max(arr[0]+arr[2], arr[1]+arr[2])
-for i in range(3,N):
-    dp[i] = max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i])
-print(dp[N-1])
+arr = [int(input()) for i in range(N)]
+if N == 1:
+    print(arr[0])
+elif N == 2:
+    print(arr[0]+arr[1])
+else:
+    dp[0] = arr[0]
+    dp[1] = arr[0] + arr[1]
+    dp[2] = max(arr[0]+arr[2], arr[1]+arr[2])
+    for i in range(3,N):
+        dp[i] = max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i])
+    print(dp[N-1])
 
 
