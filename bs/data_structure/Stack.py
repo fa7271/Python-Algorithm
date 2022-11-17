2cbeecfcdf22b11a1401252c6f8a551df32bb095
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-17 13:26:09 +0900

    221116 Stack

--- bs/data_structure/Stack.py
+++ bs/data_structure/Stack.py
@@ -1,7 +1,7 @@
-# from doublylinkedlist import Node
-# from doublylinkedlist import DoublyLinkedList
 
 # 괄호 문제
+from Python.bs.data_structure.Doubly_Linked_Lists import DoublyLinkedList
+
 
 class ArrayStack:
     def __init__(self):
@@ -21,6 +21,81 @@ class ArrayStack:
 
     def peek(self):
         return self.data[-1]
+
+def splitTokens(exprStr):
+    tokens = []
+    val = 0
+    valProcessing = False
+    for c in exprStr:
+        if c == ' ':
+            continue
+        if c in '0123456789':
+            val = val * 10 + int(c)
+            valProcessing = True
+        else:
+            if valProcessing:
+                tokens.append(val)
+                val = 0
+            valProcessing = False
+            tokens.append(c)
+    if valProcessing:
+        tokens.append(val)
+    return tokens
+
+def infixToPostfix(tokenList):
+    prec = {
+        '*': 3,
+        '/': 3,
+        '+': 2,
+        '-': 2,
+        '(': 1,
+    }
+    opStack = ArrayStack()
+    postfixList = []
+    for i in tokenList:
+        if type(i) == int:
+            postfixList.append(i)
+        elif i == '(':
+            opStack.push(i)
+        elif i == ')':
+            while opStack.peek() != '(':
+                postfixList.append(opStack.pop())
+            opStack.pop()
+        elif i in prec :
+            if opStack.isEmpty():
+                opStack.push(i)
+            else:
+                while opStack.size() > 0:
+                    if prec[opStack.peek()] >= prec[i]:
+                        postfixList.append(opStack.pop())
+                    else:
+                        break
+                opStack.push(i)
+        else:
+            postfixList += i
+    while not opStack.isEmpty():
+        postfixList.append(opStack.pop())
+    return postfixList
+
+def postfixEval(tokenList):
+    num = ArrayStack()
+    for i in tokenList:
+        if type(i) == int:
+            num.push(i)
+        else:
+            A = str(num.pop())
+            B = str(num.pop())
+            num.push(eval(B+i+A))
+    return num.pop()
+# eval(my_string)
+def solution2(expr):
+    tokens = splitTokens(expr)
+    postfix = infixToPostfix(tokens)
+    val = postfixEval(postfix)
+    return val
+# print(solution2("(1 + 2) * (3 + 4)"))
+# print(solution2("7 * (9 - (3+2))"))
+print(solution2("9/3+(10-8)"))
 
 def solution(expr):
     match = {
@@ -72,8 +147,7 @@ def solution1(S):
         answer += opstact.pop()
     return answer
 
-print(solution1("A*(B+C)"))
-# print(solution("[(A + B) * C]"))
+# print(solution1("A*(B+C)"))
 
 class LinkedListStack:
 
