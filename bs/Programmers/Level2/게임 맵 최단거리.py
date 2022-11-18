629c5882a1b84bdc5171d4a33e82b4315712880b
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-18 23:34:36 +0900

    221117 programmers 게임 맵 최단거리

---
+++ bs/Programmers/Level2/게임 맵 최단거리.py
@@ -0,0 +1,53 @@
+# from collections import deque
+#
+# def solution(maps):
+#     n, m = len(maps), len(maps[0])
+#     visited = [[-1]*m for _ in range(n)]
+#     visited[0][0] = 1
+#     move = [(0,1), (1,0), (0,-1), (-1,0)]
+#     q = deque([(0, 0)])
+#
+#     while q:
+#         x, y = q.popleft()
+#
+#         if x==n-1 and y==m-1:
+#             return visited[x][y]
+#
+#         for a, b in move:
+#             dx, dy = x+a, y+b
+#
+#             if n>dx>=0 and m>dy>=0 and maps[dx][dy] == 1 and visited[dx][dy] == -1:
+#                 q.append((dx,dy))
+#                 visited[dx][dy] = visited[x][y] + 1
+#
+#     return -1
+
+
+from collections import deque
+
+def solution(maps):
+    N, M = len(maps) , len(maps[0])
+    visited = [[-1]*M for _ in range(N)]
+    visited[0][0] = 1
+    move = [(0,1), (0,-1),(-1,0),(1,0)]
+
+    q = deque([(0,0)])
+
+    while q:
+
+        x, y = q.popleft()
+        if x == N-1 and y == M - 1:
+            return visited[x][y]
+        for a, b in move:
+            dx, dy = x+a , y+b # 들어갈 곳
+            if N > dx >=0 and M > dy >=0 and visited[dx][dy] == -1 and maps[dx][dy] == 1:
+                q.append((dx,dy))
+                visited[dx][dy] = visited[x][y] + 1
+
+    return -1
+
+
+
+
+print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 0], [0, 0, 0, 0, 1]]))
+print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 0], [0, 0, 0, 0, 1]]))
