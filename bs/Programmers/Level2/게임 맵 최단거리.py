3ef9624fb4904bd4aeaf9a2dab54e174acbf979f
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-19 16:02:26 +0900

    221118 practice

--- bs/Programmers/Level2/게임 맵 최단거리.py
+++ bs/Programmers/Level2/게임 맵 최단거리.py
@@ -49,5 +49,28 @@ def solution(maps):
 
 
 
-print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 0], [0, 0, 0, 0, 1]]))
-print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 0], [0, 0, 0, 0, 1]]))
+
+from collections import deque
+
+def solution2(maps):
+    N,M = len(maps), len(maps[0])
+
+    visited = [[-1]*M for _ in range(N)]
+    visited [0][0] = 1 # 시작점은  밟아서 1
+    move = [(0,1),(0,-1),(1,0),(-1,0)]
+
+    q = deque([(0,0)]) # 시작점
+
+    while q:
+        x, y = q.popleft()
+        if x == N-1 and y == M-1 :
+            return visited[x][y]
+        for a, b in move:
+            x_move, y_move = x + a, y + b
+            if N > x_move >=0 and M > y_move >= 0 and visited[x_move][y_move] == -1 and maps[x_move][y_move] == 1:
+                q.append((x_move,y_move))
+                visited[x_move][y_move] = visited[x][y] + 1
+    return -1
+
+
+print(solution2([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 0], [0, 0, 0, 0, 1]]))
---
+++ bs/practice.py
@@ -0,0 +1,32 @@
+import sys, heapq
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
+
+input = sys.stdin.readline
+V, E = map(int,input().split()) # 점 5개 간선 6개
+start = int(input()) # 시작점 1
+graph =[[] for _ in range(V+1)]
+dist = [float('inf')] * (V+1)
+dist[start] = 0
+
+for i in range(E):
+    u,v,w = map(int,input().split())
+    graph[u].append((v,w))
+
+heap = []
+heapq.heappush(heap,(0,start))
+
+while heap:
+    cur_w, cur_node = heapq.heappop(heap)
+
+    for to_node, to_w in graph[cur_node]:
+        dis = cur_w + to_w
+        if dis < dist[to_node]:
+            dist[to_node] = dis
+            heapq.heappush(heap,(dis, to_node))
+
+for i in dist[1:]:
+    print(i if i!=float('inf') else 'INF')
+
+
+
+
