fc71c91a17c453c4e1e98260432cf22d77b6cd14
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-20 21:48:43 +0900

    221120 15666 bt

---
+++ bs/백트래킹/15666.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
+
+
+n,m = map(int,input().split())
+x = sorted(list(map(int,input().split())))
+result = []
+
+def bt():
+    if m == len(result):
+        print(*result)
+        return
+    now = 0
+    for i in range(n):
+        if now != x[i]:
+            if any(x[i] < num for num in result):
+                continue
+            result.append(x[i])
+            now = x[i]
+            bt()
+            result.pop()
+bt()
