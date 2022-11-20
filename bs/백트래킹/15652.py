43c172a7908f8da225c0983a85d59ed238696d77
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-20 13:38:11 +0900

    221120 baeck bt 15652

--- bs/백트래킹/15652.py
+++ bs/백트래킹/15652.py
@@ -1,25 +1,35 @@
 import sys
-sys.stdin = open('/Python/h.txt', 'r')
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 
 
 n,m = map(int,input().split())
-visited = [False] * n
 result = []
 
-def solve(num):
-    if num == m:
+# def solve(num):
+#     if num == m:
+#         print(*result)
+#         return
+#
+#     for i in range(n):
+#         if visited[i] == False:
+#             # visited[i] = True
+#             result.append(i + 1)
+#             solve(num+1)
+#             visited[i] = True
+#             result.pop()
+#             for j in range(i+1, n):
+#                 visited[j] = False
+#
+# solve(0)
+
+def bt(num):
+    if len(result) == m:
         print(*result)
         return
 
-    for i in range(n):
-        if visited[i] == False:
-            # visited[i] = True
-            result.append(i + 1)
-            solve(num+1)
-            visited[i] = True
-            result.pop()
-            for j in range(i+1, n):
-                visited[j] = False
+    for i in range(num, n+1):
+        result.append(i)
+        bt(i)
+        result.pop()
 
-solve(0)
-
+bt(1)
