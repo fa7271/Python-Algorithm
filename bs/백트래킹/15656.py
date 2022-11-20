0e018d44267eddfaa60a8ba6576677417b8db040
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-20 14:42:56 +0900

    221120 baeck bt 15656

---
+++ bs/백트래킹/15656.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
+
+
+n,m = map(int,input().split())
+x = sorted(list(map(int,input().split())))
+result = []
+
+def bt(num):
+    if m == len(result):
+        print(*result)
+        return
+    for i in range(n):
+        result.append(x[i])
+        bt(num+1)
+        result.pop()
+bt(0)
+
+
+# def backtracking(depth):
+#     if depth == M:
+#         print(' '.join(map(str,box)))
+#         return
+#
+#     for i in range(N):
+#         if numbers[i] in box:
+#             continue
+#         box.append(numbers[i])
+#         backtracking(depth + 1)
+#         box.pop()
+#
+# box = []
