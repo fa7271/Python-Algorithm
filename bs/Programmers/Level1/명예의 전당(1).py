2c9a1d95c1ecf57f9b31c70df665866b41769c18
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-11-27 22:12:26 +0900

    221127 명예의전당

--- bs/Programmers/Level1/명예의 전당(1).py
+++ bs/Programmers/Level1/명예의 전당(1).py
@@ -1,18 +1,16 @@
 import heapq
 def solution(k, score):
 
-    lst =[]
-    heapq.heapify(lst)
-    res = []
+    save, res = [], []
     for i in score:
-        if len(lst) <k:
-            heapq.heappush(lst,i)
-            res.append(lst[0])
+        if len(save) < k:
+            heapq.heappush(save,i)
         else:
-            x = heapq.heappop(lst)
-            if i >= x:
-                res.append(x)
-                heapq.heappush(lst,i)
-    print(res)
+            heapq.heappushpop(save,i)
+        res.append(save[0])
+    return res
+
+
+
 print(solution(3, [10, 100, 20, 150, 1, 100, 200]))
 # print(solution(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]))
