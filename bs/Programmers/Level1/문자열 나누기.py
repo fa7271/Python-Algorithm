65a40f8aaf67f85fc67f3838828b9566b03409b4
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-12-02 15:11:08 +0900

    221202 programmers LV1문자열 나누기

---
+++ bs/Programmers/Level1/문자열 나누기.py
@@ -0,0 +1,20 @@
+from collections import deque
+def solution(s):
+    x = deque([i for i in (s)])
+    main, count = ["", 0, 0,], 0
+    for i in x:
+        if main[0] =="":
+            main[0] += i
+            main[1] +=1
+        else:
+            if main[0] == i:
+                main[1] += 1
+            else :
+                main[0] == i
+                main[2] +=1
+
+            if main[1] ==main[2]:
+                count += 1
+                main = ["", 0, 0]
+    if main != ["", 0, 0] : count+=1
+    return count
