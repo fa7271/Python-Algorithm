a4e5dfa7d3533590a825e4a2214e0cc924f0e9d0
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-12-02 17:47:13 +0900

    221202 programmers LV2 타겟넘버 dfs

--- bs/Programmers/Level2/타겟넘버.py
+++ bs/Programmers/Level2/타겟넘버.py
@@ -33,4 +33,25 @@ def bfs (numbers, target):
     return res
 
 
-print(bfs([1, 1, 1, 1, 1], 3))
+# print(bfs([1, 1, 1, 1, 1], 3))
+
+
+def solution1(numbers, target):
+    answer = dfs(numbers,target,0)
+    return answer
+
+def dfs(numbers,target, depth):
+
+    answer = 0
+    if len(numbers) == depth:
+        if sum(numbers) == target:
+            return 1
+        else: return 0
+
+    else:
+        answer += dfs(numbers, target, depth+1)
+        numbers[depth] *= -1
+        answer += dfs(numbers, target, depth+1)
+        return answer
+
+print(solution1([1,1,1,1,1], 3))
