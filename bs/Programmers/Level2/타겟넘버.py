bd74484dc1855052ac8f224379f2660e9beca227
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-12-02 18:10:29 +0900

    221202 programmers LV2 타겟넘버 dfs

--- bs/Programmers/Level2/타겟넘버.py
+++ bs/Programmers/Level2/타겟넘버.py
@@ -54,4 +54,18 @@ def dfs(numbers,target, depth):
         answer += dfs(numbers, target, depth+1)
         return answer
 
+def solution(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0,0)
+    return answer
 print(solution1([1,1,1,1,1], 3))
