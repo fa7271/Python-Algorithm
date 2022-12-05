c41b260749e8863b0af797eea63bfce47826ab6f
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-12-05 20:52:41 +0900

    221205 백준 14888 연산

--- bs/백트래킹/14888.py
+++ bs/백트래킹/14888.py
@@ -4,45 +4,26 @@ sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 N = int(input())
 number = list(map(int,input().split()))
 op = list(map(int,input().split()))
+max_res, min_res = -sys.maxsize -1, sys.maxsize
 
-res = number[0]
+def solution(num,idx,a,b,c,d):
+    global max_res,min_res
 
-max_value = ""
-min_value = ""
+    if idx == N:
+        max_res = max(max_res, num)
+        min_res = min(min_res, num)
+    if a > 0:
+        solution(num + number[idx], idx+1, a-1, b, c, d)
+    if b > 0:
+        solution(num - number[idx], idx+1, a, b-1, c, d)
+    if c > 0:
+        solution(num * number[idx], idx+1, a, b, c-1, d)
+    if d > 0:
+        solution(int(num / number[idx]), idx+1, a, b, c, d-1)
 
-def dfs(num):
-    global max_value, min_value, res
-    if sum(op) == 0:
-        if min_value =="":
-            min_value = res
-        elif min_value > res:
-            min_value = res
 
-        if max_value =="":
-            max_value = res
-        elif max_value < res:
-            max_value = res
-        return
-
-    for i in range(4):
-        if op[i] > 0:
-            now = res
-            if i == 0:
-                now += number[num]
-            elif i == 1:
-                res -= number[num]
-            elif i == 2:
-                res *= number[num]
-            else:
-                res = int(res/number[num])
-
-            op[i] -= 1
-            dfs(num + 1)
-            res = now
-            op[i] += 1
-
-dfs(1)
-print(min_value,max_value)
+solution(number[0], 1,op[0], op[1], op[2], op[3])
+print(max_res,min_res, sep='\n')
 
 
 
