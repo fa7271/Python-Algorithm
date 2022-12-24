44db258ba092ae3582e04d0e3f604d52915f5c4b
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-12-24 22:38:58 +0900

    221224

--- bs/Programmers/Level2/쿼드 압축 후 갯수 세기.py
+++ bs/Programmers/Level2/쿼드 압축 후 갯수 세기.py
@@ -4,10 +4,10 @@ def check(x, y, n, arr):
             return [0, 1]
         else:
             return [1, 0]
-    left_up = check(x, y , n // 2, arr)
-    right_up = check(x, y + n // 2, n // 2, arr)
-    left_down = check(x + n // 2, y, n // 2, arr)
-    right_down = check(x + n // 2, y + n // 2, n // 2, arr)
+    left_up = check(x, y , n // 2, arr) #왼쪽
+    right_up = check(x, y + n // 2, n // 2, arr)#오른쪽
+    left_down = check(x + n // 2, y, n // 2, arr)#왼쪽아래
+    right_down = check(x + n // 2, y + n // 2, n // 2, arr)#오른쪽아래
 
     if left_up == right_up == left_down == right_down == [0, 1] or left_up == right_up == left_down == right_down == [1, 0]:
         return left_up
