ec7e2dfc504171338a4355d44a55916945e6b9e0
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-12-26 16:35:30 +0900

    221226 테이블 해시 함수

---
+++ bs/Programmers/Level2/테이블 해시 함수.py
@@ -0,0 +1,20 @@
+def solution(data, col, row_begin, row_end):
+
+    x = sorted(data, key =lambda x:(x[col-1],-x[0]))
+    res = []
+    while row_begin<=row_end:
+        ans = 0
+        for i in x[row_begin-1]:
+            ans += i % row_begin
+
+        res.append(ans)
+        row_begin += 1
+    result = res[0]
+    for i in res[1:]:
+        result = result ^ i
+    return result
+    # print(result)
+
+
+
+print(solution([[2, 2, 6], [1, 5, 10], [4, 2, 9], [3, 8, 3]], 2, 2, 3))
