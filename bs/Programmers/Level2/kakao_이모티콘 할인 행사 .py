a9a22a08f94ebbb002c0c8b0a3bba51308e2ea2a
fa7271 <90543205+fa7271@users.noreply.github.com>
2023-01-17 15:38:31 +0900

    230117 programmers kakao_이모티콘 할인 행사

--- bs/Programmers/Level2/kakao_이모티콘 할인 행사 .py
+++ bs/Programmers/Level2/kakao_이모티콘 할인 행사 .py
@@ -3,20 +3,20 @@ def solution(users, emoticons):
 
     res = [0, 0]
     for discounts in product((40,30,20,10), repeat = len(emoticons)):
+        # print(discounts)
         result = [0, 0] # 이모티콘 판매액
         for user_discount, user_money in users:
-            sell = 0
+            # print(user_discount,user_money)
+            sub_pass = 0
+            # print(discounts)
             for emoticon, discount in zip(emoticons,discounts):
-                print(emoticon,discount)
                 if discount >= user_discount:
-                    sell += emoticon * (1-discount/100)
-            if sell >= user_money:
+                    sub_pass += emoticon * (1-discount/100)
+            if sub_pass >= user_money:
                 result[0] += 1
             else:
-                result[1] += sell
+                result[1] += sub_pass
         res = max(res, result)
     return res
-# 할인율 10%, 20%, 30%, 40%
-
 print(solution([[40, 10000], [25, 10000]], [7000, 9000]))
 # print(solution([[40, 2900], [23, 10000], [11, 5200], [5, 5900], [40, 3100], [27, 9200], [32, 6900]], [1300, 1500, 1600, 4900]))
