13984a2632c7b0c47b7b0177b56217fce09ecec9
fa7271 <90543205+fa7271@users.noreply.github.com>
2023-02-12 13:33:29 +0900

    230212 programmers_spring_coding_test

--- bs/Programmers/Level2/숫자 변환하기.py
+++ bs/Programmers/Level2/숫자 변환하기.py
@@ -1,9 +1,23 @@
 def solution(x, y, n):
+    # count = 0
+    # if x == n :
+    #     return count
+    # if x > n :
+    #     return -1
+    # else: return -1
     count = 0
-    if x == n :
-        return count
-    if x > n :
+    while y > x:
+        y -= n
+        if y <= n:
+            break
+        else:
+        count += 1
+        if x*3 == y:
+            break
+        if x*2 ==y:
+            break
+    if count == 0:
         return -1
-    else: return -1
-
-print(solution(	10, 40, 5))
+    else: return count
+print(solution(	10, 40, 30))
+print(solution(	2, 5, 4))
