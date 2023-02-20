b0dd31a19ad80d9b853feb68e596950030218311
fa7271 <90543205+fa7271@users.noreply.github.com>
2023-02-20 15:36:45 +0900

    230220 programmers LV2 숫자 변환하기

--- bs/Programmers/Level2/숫자 변환하기.py
+++ bs/Programmers/Level2/숫자 변환하기.py
@@ -23,28 +23,27 @@ def solution(x, y, n):
     bfs(x,y,n)
 
     return board[y]-1
-
-# def solution(x, y, n):
-#
-#     count = 0
-#     answer = set()
-#     answer.add(x) # 시작값 x 10 넣음
-#
-#     while answer:
-#         print(answer)
-#         if y in answer:
-#             return count
-#
-#         nx_answer = set()
-#         for nx in answer:
-#             if nx + n <= y:
-#                 nx_answer.add(n+nx)
-#             if nx * 2 <= y:
-#                 nx_answer.add(2*nx)
-#             if nx * 3 <= y:
-#                 nx_answer.add(3*nx)
-#         answer = nx_answer
-#         count += 1
-#     return -1
-
 print(solution(10,40,30))
+def solution2(x, y, n):
+
+    count = 0
+    answer = set()
+    answer.add(x) # 시작값 x 10 넣음
+
+    while answer:
+        if y in answer:
+            return count
+
+        nx_answer = set()
+        for nx in answer:
+            if nx + n <= y:
+                nx_answer.add(n+nx)
+            if nx * 2 <= y:
+                nx_answer.add(2*nx)
+            if nx * 3 <= y:
+                nx_answer.add(3*nx)
+        answer = nx_answer
+        count += 1
+    return -1
+
+print(solution2(10,40,30))
