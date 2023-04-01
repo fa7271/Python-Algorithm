7d4b2e1429cb571993a840ba8f1c4c233692d780
fa7271 <90543205+fa7271@users.noreply.github.com>
2023-04-01 17:11:04 +0900

    230325 programmers LV1 공원산책

---
+++ bs/Programmers/Level1/공원 산책.py
@@ -0,0 +1,55 @@
+def solution(park, routes):
+    answer = []
+    start = []
+
+    for i in range(len(park)):
+        if 'S' in park[i]:
+            start = [i, park[i].find('S')]
+            break
+    print(start)
+    for route in routes:
+        dir, move = route.split(' ')
+        move = int(move)
+
+        if dir == 'E':
+            loc = start[1] + move
+            if loc >= len(park[0]):
+                continue
+            if 'X' in park[start[0]][start[1]+1:loc+1]:
+                continue
+            else:
+                start[1] = loc
+
+        elif dir == 'W':
+            loc = start[1] - move
+            if loc < 0:
+                continue
+            if 'X' in park[start[0]][loc:start[1]]:
+                continue
+            else:
+                start[1] = loc
+
+        elif dir == 'S':
+            loc = start[0] + move
+            if loc >= len(park):
+                continue
+            if 'X' in [park[i][start[1]] for i in range(start[0]+1,loc+1)]:
+                continue
+            else:
+                start[0] = loc
+
+        elif dir == 'N':
+            loc = start[0] - move
+            if loc < 0:
+                continue
+            if 'X' in [park[i][start[1]] for i in range(loc,start[0])]:
+                continue
+            else:
+                start[0] = loc
+
+    return start
+
+
+print(solution(	["SOO", "OXX", "OOO"], ["E 2", "S 2", "W 1"]))
+# print(solution(["OSO", "OOO", "OXO", "OOO"], ["E 2", "S 3", "W 1"]))
+
