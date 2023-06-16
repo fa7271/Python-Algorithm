67f75c925bf02b54bc71ef608c60f2273e2a810e
fa7271 <90543205+fa7271@users.noreply.github.com>
2023-06-16 14:52:22 +0900

    230615 boj 1780

---
+++ bs/분할정복/1780.py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
+
+
+def dfs(x,y,z):
+    global answer
+    now = board[x][y]
+
+    for i in range(x, x+z):
+        for j in range(y, y+z):
+            if now != board[i][j]:
+                for k in range(3):
+                    for l in range(3):
+                        dfs(x + k*z//3, y + l*z//3, z//3)
+                return
+    if now == -1:
+        answer[0] += 1
+    elif now == 0:
+        answer[1] += 1
+    else:
+        answer[2] +=1
+
+n = int(sys.stdin.readline())
+board= [list(map(int,sys.stdin.readline().split(" "))) for _ in range(n)]
+answer = [0,0,0]
+dfs(0,0,n)
+[print(res) for res in answer]
+
