00d377ff03e72af7672b714274ceaf644ce1808f
fa7271 <90543205+fa7271@users.noreply.github.com>
2023-06-16 18:37:14 +0900

    230616 boj 10830

---
+++ bs/분할정복/10830.py
@@ -0,0 +1,39 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
+
+N, M = map(int,sys.stdin.readline().split(" "))
+arr = [list(map(int,sys.stdin.readline().rstrip().split(" "))) for _ in range(N)]
+
+def solution(arr,M):
+    if M == 1: # 1 곱할때는 그냥 리턴
+        return arr
+
+    ar = solution(arr, M//2)
+    cal = []
+    for i in range(N):
+        result = []
+        for l in range(N):
+            a = 0
+            for j in range(N):
+                a += ar[i][j] * ar[j][l]
+            result.append(a%1000)
+        cal.append(result)
+
+    answer = []
+    if M % 2 == 1:
+        for i in range(N):
+            result = []
+            for l in range(N):
+                a = 0
+                for j in range(N):
+                    a += cal[i][j] * arr[j][l]
+                result.append(a%1000)
+            answer.append(result)
+    else:
+        answer = cal
+    return answer
+
+for i in solution(arr,M):
+    for j in i:
+        print(j, end =" ")
+    print()
