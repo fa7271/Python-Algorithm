bb0ea57bfecf3c5c000eea698d0eaabd9dadaf2a
fa7271 <90543205+fa7271@users.noreply.github.com>
2023-06-18 01:08:43 +0900

    230616 boj 신입사원 교육, 카페확장

--- bs/분할정복/1780.py
+++ bs/분할정복/1780.py
@@ -8,11 +8,12 @@ def dfs(x,y,z):
 
     for i in range(x, x+z):
         for j in range(y, y+z):
-            if now != board[i][j]:
+            if now != board[i][j]: # 1개까지 dfs돌고 1개일때 여기서 걸리면서
                 for k in range(3):
                     for l in range(3):
                         dfs(x + k*z//3, y + l*z//3, z//3)
                 return
+    # 여기서 체크해주고 리턴함
     if now == -1:
         answer[0] += 1
     elif now == 0:
