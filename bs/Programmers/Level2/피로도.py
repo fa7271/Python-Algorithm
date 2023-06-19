f97238a896cab46cca95ca2085495c209ce16547
fa7271 <90543205+fa7271@users.noreply.github.com>
2023-06-19 14:15:11 +0900

    230619 boj 27433

--- bs/Programmers/Level2/피로도.py
+++ bs/Programmers/Level2/피로도.py
@@ -6,6 +6,7 @@ def solution(k, dungeons):
     # 방문할 순서 조합 만들기
     numbers = list(x for x in range(0,len(dungeons)))
     cases = permutations(numbers,len(dungeons))
+    print(list(cases),numbers)
 
     results = []
     # 각 조합에 대해
@@ -23,3 +24,28 @@ def solution(k, dungeons):
         results.append(count)
     return max(results)
 print(solution(80, [[80, 20], [50, 40], [30, 10]]))
+
+
+answer = 0
+N = 0
+visited = []
+
+
+def dfs(k, cnt, dungeons):
+    global answer
+    if cnt > answer:
+        answer = cnt
+
+    for j in range(N):
+        if k >= dungeons[j][0] and not visited[j]:
+            visited[j] = 1
+            dfs(k - dungeons[j][1], cnt + 1, dungeons)
+            visited[j] = 0
+
+
+def solution(k, dungeons):
+    global N, visited
+    N = len(dungeons)
+    visited = [0] * N
+    dfs(k, 0, dungeons)
+    return answer
