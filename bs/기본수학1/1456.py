f8fe044f3de36ec68920c3e77c9b896d30970348
fa7271 <90543205+fa7271@users.noreply.github.com>
2023-06-22 15:48:25 +0900

    230620 boj 1456

---
+++ bs/기본수학1/1456.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
+
+A, B = map(int,sys.stdin.readline().split(" "))
+
+board = [True] * (int(B ** 0.5) + 1)
+board[1] = False
+m = int(B **0.5)
+for i in range(2,m+1):
+    if board[i]:
+        for j in range(i**2,m+1,i):
+            board[j] = False
+
+result = 0
+for i in range(1, len(board)):
+    if board[i]:
+        res = i * i
+        while True:
+            print(i,res,result)
+            if res < A:
+                res *= i
+                continue
+            elif res > B:
+                break
+            res *= i
+            result += 1
+print(result)
+
+
