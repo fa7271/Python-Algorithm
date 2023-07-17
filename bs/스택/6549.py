c9a5f1b7ee63044da58b3b85fe4bb2dc9396f37a
fa7271 <90543205+fa7271@users.noreply.github.com>
2023-07-17 14:08:47 +0900

    230717 6549

--- bs/스택/6549.py
+++ bs/스택/6549.py
@@ -10,14 +10,16 @@ while True:
     max_area = 0  # 가장 큰 직사각형의 넓이
     for i in range(n):
         while stack and heights[i] < heights[stack[-1]]: # 스택이 있고, 다음 건물 높이가 스택 마지막 보다 낮을때까지
-            height = heights[stack.pop()] # 높이는 스택 마지막 건물 높
+            height = heights[stack.pop()] # 높이는 스택 마지막 건물 높이
             width = i if not stack else i - stack[-1] - 1 # i번째 까지의 최소 높이
             area = height * width # 면적
             max_area = max(max_area, area) # 최대 면적 구해준다.
         stack.append(i) # 스택이 비어있으면
+    print(stack)
     while stack: # 스택이 남아있으며 예제2번같이 똑같이 해준다.
         height = heights[stack.pop()]
         width = n if not stack else n - stack[-1] - 1
         area = height * width
         max_area = max(max_area, area)
 
+    print(max_area)
