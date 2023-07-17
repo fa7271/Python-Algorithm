e132f4bac0ae58d6f03a74981adcba952f4301ef
fa7271 <90543205+fa7271@users.noreply.github.com>
2023-07-17 15:17:16 +0900

    230717 11437

--- bs/그래프와순회/11437.py
+++ bs/그래프와순회/11437.py
@@ -1,33 +1,51 @@
 
 import sys
+sys.setrecursionlimit(100000)
 sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 
-def find_parent(parent, x):
-    result = [x]
-    while parent[x]:
-        result.append(parent[x])
-        x = parent[x]
-    return result
+
+N = int(sys.stdin.readline()) # 16
+graph = [[] for _ in range(N+1)]
 
 
-result = []
-N = int(input()) # 16
-parent = [0 for _ in range(N+1)]
+# 양방향 노드 설정
 for i in range(N-1):
-    x, y = map(int, sys.stdin.readline().rstrip().split(" ")) # x 가 y의 부모
-    parent[y] = x
-print(parent)
-m = int(sys.stdin.readline())
-for _ in range(m):
     x, y = map(int, sys.stdin.readline().rstrip().split(" "))
-    lst_a = find_parent(parent,x)
-    lst_b = find_parent(parent,y)
-    if len(lst_a) > len(lst_b):
-        lst_a = lst_a[-len(lst_b):]
-    else:
-        lst_b = lst_b[-len(lst_a):]
-    print(lst_a,lst_b)
-#     depth = 0
-#     while lst_a[depth] != lst_b[depth]:
-#         depth += 1
-#     result.append(depth)
+    graph[x].append(y)
+    graph[y].append(x)
+
+parent = [0] * (N + 1)      # 각 노드의 부모 노드 정보
+depth = [0] * (N + 1)       # 각 노드까지의 깊이
+visited = [0] * (N + 1)     # 방문 여부
+def dfs(x, idx):
+    visited[x] = 1 # 방문처리해주고
+    depth[x] = idx # 방문했으니 현재위치의 depth를 idx로 해줌 기본은 0
+    for node in graph[x]: # 그 다음 방문위치
+        if visited[node] == 1: # 이미 방문했으면
+            continue # 넘어가고
+        parent[node] = x # 그렇지 않으면 자식들이니깐 자식들의 부모는 내가 됨
+        dfs(node, idx + 1) # 깊이탐색출발 깊이는 1 늘림
+dfs(1,0)
+
+def find_parent(left, right):
+
+    # 깊이를 맞춰줌
+    while depth[left] != depth[right]: # 깊이가 다르다.
+        if depth[left] > depth[right]: # 왼쪽이 더 깊으면
+            left = parent[left] # 깊이가 맞을때까지 부모로 올라옴
+        else:
+            right = parent[right] # 반대도 동일
+    # 노드 맞추기
+    while left != right: # 노드가 다르면
+        left = parent[left] # 노드의 부모로 올라옴
+        right = parent[right] # 노드의 부모로 올라옴
+    # 주의할점 4의 조상이 2 일때 2와 4 의 공통부모는 2이다. 2는 자기자신이 부모인것이다.
+
+    return left # 맞으면 리턴
+
+# 문제 출력
+m = int(sys.stdin.readline())
+
+for i in range(m):
+    left, right = map(int,sys.stdin.readline().split(" "))
+    print(find_parent(left,right))
