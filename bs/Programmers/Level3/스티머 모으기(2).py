f3414c77220d2d433432b43cf3d5d8c8f7e138e0
boseok <fa7271@naver.com>
2024-01-02 10:06:35 +0900

    240102 programmers 스티커 모으기(2)

--- bs/Programmers/Level3/스티머 모으기(2).py
+++ bs/Programmers/Level3/스티머 모으기(2).py
@@ -1,9 +1,9 @@
 from collections import deque
 
+
 # 너무 dp 같은데.
 # 첫번째 뜯었을경우, 안 뜯었을 경우.
 def solution(sticker):
-
     # 시도 1
     # answer = []
     # for j in range(2):
@@ -14,18 +14,26 @@ def solution(sticker):
     # return max(answer)
 
     # 시도2.
-    if len(sticker)<= 2:
+    if len(sticker) <= 2:
         return max(sticker)
-    dp = [-1e9] * len(sticker)
+
+    dp = [0] * len(sticker)
+    no_dp = [0] * len(sticker)
+
     dp[0] = sticker[0]
-    dp[1] = sticker[1]
-    for i in range(3,len(sticker)):
-        dp[i] = max(dp[i-2] + sticker[i], dp[i-1])
-# 시도3.
+    dp[1] = dp[0]
+
+    # 맨 앞 스티커 뜯은 경우 >> 끝에꺼는 못 뜯음 -> dp[-2] 최대
+    for i in range(2, len(sticker) - 1):
+        dp[i] = max(dp[i - 2] + sticker[i], dp[i - 1])
+
+    # 맨 앞 스티커 안 뜯은경우 > 끝에꺼 뜯음 > no_dp[-1]
+    for i in range(1, len(sticker)):
+        no_dp[i] = max(no_dp[i - 2] + sticker[i], no_dp[i - 1])
+    return max(no_dp[-1], dp[-2])
 
 
-    return max(dp)
 # print(solution([14, 6, 5, 11, 3, 9, 2, 10]	)) #  36
-print(solution([1, 3, 2, 5, 4] )) # 8
+print(solution([1, 3, 2, 5, 4]))  # 8
 
 # sticker = 1~100,000
