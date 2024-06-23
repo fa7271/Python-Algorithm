23cb86009f9345de248a01347a879494b5e7554f
boseok <fa7271@naver.com>
2024-06-23 10:46:12 +0900

    240622 1699

--- bs/동적 계획법/1699.py
+++ bs/동적 계획법/1699.py
@@ -12,7 +12,7 @@ for i in range(1, n + 1):
     if math.isqrt(i) ** 2 == i:
         dp[i] = 1
     else:
-        for j in range(1, int(i ** 0.5) + 1):
+        for j in range(1, int(i ** 0.5) + 1) :
             if dp[i] > (dp[i - j * j] + 1):
                 dp[i] = dp[i - j * j] + 1
 print(dp[n])
