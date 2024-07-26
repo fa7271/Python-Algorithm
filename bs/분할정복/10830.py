4c75095dd7ff63765c0adec075da3b1b91b69548
boseok <fa7271@naver.com>
2024-07-26 16:09:44 +0900

    240726 10830

--- bs/분할정복/10830.py
+++ bs/분할정복/10830.py
@@ -2,15 +2,14 @@ import sys
 
 sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 
-N, M = map(int, sys.stdin.readline().split(" "))
-arr = [list(map(int, sys.stdin.readline().rstrip().split(" "))) for _ in range(N)]
+N, M = map(int,sys.stdin.readline().split(" "))
+arr = [list(map(int,sys.stdin.readline().rstrip().split(" "))) for _ in range(N)]
 
-
-def solution(arr, M):
-    if M == 1:  # 1 곱할때는 그냥 리턴
+def solution(arr,M):
+    if M == 1: # 1 곱할때는 그냥 리턴
         return arr
 
-    ar = solution(arr, M // 2)
+    ar = solution(arr, M//2)
     cal = []
     for i in range(N):
         result = []
@@ -18,28 +17,28 @@ def solution(arr, M):
             a = 0
             for j in range(N):
                 a += ar[i][j] * ar[j][l]
-            result.append(a % 1000)
+            result.append(a%1000)
         cal.append(result)
 
     answer = []
-    if M % 2 == 1:
+    if M % 2:
         for i in range(N):
             result = []
             for l in range(N):
                 a = 0
                 for j in range(N):
                     a += cal[i][j] * arr[j][l]
-                result.append(a % 1000)
+                result.append(a%1000)
             answer.append(result)
     else:
         answer = cal
     return answer
 
-
-for i in solution(arr, M):
+for i in solution(arr,M):
     for j in i:
-        print(j, end=" ")
+        print(j%1000, end =" ")
     print()
+
 
 
 def 행렬곱셈(A, B, n):
