2b6c63db8cc1686e227b8e8d0d9a1818f5daacc9
boseok <fa7271@naver.com>
2024-08-09 22:32:39 +0900

    240809 11049

--- bs/동적 계획법/11049.py
+++ bs/동적 계획법/11049.py
@@ -3,16 +3,15 @@ import sys
 sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 n = int(input())
 
-graph = []
-
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+graph = [list(map(int, sys.stdin.readline().rstrip().split(" "))) for _ in range(n)]
 
 dp = [[0] * n for _ in range(n)]
-for i in range(1, n):  # i는 간격에 따른 대각선 줄을 의미한다.
-    for j in range(n - i):  # j는 대각선의 항목들
+
+for i in range(1, n):
+    for j in range(n - i):
         x = i + j
         dp[j][x] = 2 ** 32
-        for k in range(j, x):  # 나올 수 있는 모든 최솟값을 구한다.
+        for k in range(j, x):
+            print(k, graph[j][0],graph[k][1],graph[x][1])
             dp[j][x] = min(dp[j][x], dp[j][k] + dp[k + 1][x] + graph[j][0] * graph[k][1] * graph[x][1])
-print(dp[0][n - 1])
+print(dp[0][-1])
