c57768b526a48025e435464e9e7e866d8bb167c8
boseok <fa7271@naver.com>
2024-08-10 11:37:24 +0900

    240810 1783

---
+++ bs/그리디/1783.py
@@ -0,0 +1,16 @@
+import sys, time
+
+sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
+
+n, m = map(int, input().split())
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(min(4, (m + 1) // 2))
+else:
+    if m <= 6:
+        print(min(4, m))
+    else:
+        print(m-2)
+
