500c88d789b309f4b248940be9a81760eff975b6
boseok <fa7271@naver.com>
2024-09-13 08:59:09 +0900

    240913 4803

--- bs/그리디 알고리즘/1758.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
-
-n = int(input())
-tips =sorted([int(input()) for x in range(n)], reverse=True)
-res = [x for x in enumerate(tips)]
-print(sum([(y-x) for x,y in res if y - x > 0]))
