b06c1e1b84d9de18ff3d6183641026b70a6fc75b
boseok <fa7271@naver.com>
2024-10-04 12:01:06 +0900

    241004 충돌위험 찾기

--- bs/Programmers/Level2/충돌위험 찾기.py
+++ bs/Programmers/Level2/충돌위험 찾기.py
@@ -1,49 +1,45 @@
-from collections import  Counter
+from collections import Counter
 
 
 def solution(points, routes):
     def bfs(route):
         idx = 0
-        sx, sy = spots[route[0] - 1]
-        for i in range(1, len(route)):
-            ex, ey = spots[route[i] - 1]
-            if idx == 0:
-                second[idx].append((sx, sy))
+        pa = []
+        for i in range(len(route) - 1):
+            sx, sy = spots[route[i] - 1]
+            ex, ey = spots[route[i + 1] - 1]
 
             # x 좌표 맞추기
             while sx != ex:
+                pa.append((sx, sy, idx))
                 if sx < ex:
                     sx += 1
                 else:
                     sx -= 1
                 idx += 1
-                if idx < len(second):  # 인덱스 범위 확인
-                    second[idx].append((sx, sy))
 
             # y 좌표 맞추기
             while sy != ey:
+                pa.append((sx, sy, idx))
                 if sy < ey:
                     sy += 1
                 else:
                     sy -= 1
                 idx += 1
-                if idx < len(second):  # 인덱스 범위 확인
-                    second[idx].append((sx, sy))
-            sx, sy = ex, ey
+        pa.append((sx, sy, idx))
+        return pa
 
     spots = [i for i in points]
-    second = [[] for _ in range(201)]
+    second = []
 
     for route in routes:
-        bfs(route)
+        second.extend(bfs(route))
     res = 0
-    for i in second:
-        if i:
-            temp = len(set(i))
-            if temp != len(i):
-                count = Counter(i)
-                result = [key for key, value in count.items() if value >= 2]
-                res += len(result)
+    temp = Counter(second)
+    for i in temp.values():
+        if i >= 2:
+            res += 1
 
     return res
+
 
