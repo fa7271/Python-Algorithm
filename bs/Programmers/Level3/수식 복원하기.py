7b2b2ae97b2c7bd47a755faec29072e61c1f8d4c
boseok <fa7271@naver.com>
2024-10-07 23:20:40 +0900

    241007 수식복원하기

--- bs/Programmers/Level3/수식 복원하기.py
+++ bs/Programmers/Level3/수식 복원하기.py
@@ -1,42 +1,87 @@
+# 10진수 값을 base 진법으로 변환하는 함수
+def to_base(n, base):
+    if n == 0:
+        return "0"
+    digits = []
+    while n > 0:
+        digits.append(str(n % base))
+        n //= base
+    return ''.join(digits[::-1])
+
+
 def valid_result(base, expression):
-    # expression의 결과를 계산하여 반환하는 함수
-    if '+' in expression:
-        a, b = expression.split(' + ')
-        return (int(a, base) + int(b, base))
-    elif '-' in expression:
-        a, b = expression.split(' - ')
-        return (int(a, base) - int(b, base))
+    # expression을 공백으로 분리
+    ls = expression.split(" ")
+    a = int(ls[0], base)  # 첫 번째 숫자
+    b = int(ls[2], base)  # 두 번째 숫자
+    operator = ls[1]  # 연산자
+    # 결과 값이 X인 경우 (X 값을 구해야 하는 상황)
+    if ls[4] == "X":
+        if operator == "+":
+            result = a + b
+        elif operator == "-":
+            result = a - b
+        return to_base(result, base)  # 계산 결과를 base 진법으로 변환하여 반환
+
+    # 결과 값이 주어진 경우 (정답 검증)
+    else:
+        c = int(ls[4], base)  # 결과 값
+        if operator == "+" and a + b == c:
+            return base
+        elif operator == "-" and a - b == c:
+            return base
+    return False
+
 
 def solution(expressions):
-    results = []
+    number_stk = []
+    calculate_stk = []
+    number = ""
+    incorrect_expression = []
+    correct_expression = []
+    # 시작 할 base 값 구하기
 
     for expression in expressions:
-            # 지워진 값이 있는 수식 처리
-            left, right = expression.split(' = ')
-            print(left,right)
-            found = set()  # 가능한 결과를 저장할 집합
+        if "X" in expression:
+            incorrect_expression.append(expression)
+        else:
+            correct_expression.append(expression)
+        express, res = expression.replace(" ", "").split("=")
 
-            # 2진법부터 9진법까지 검토
-            for base in range(2, 10):
-                try:
-                    calculated = valid_result(base, left)
-                    if 0 <= calculated < base:  # 결과가 진법의 범위 안에 있어야 함
-                        found.add(calculated)
-                except ValueError:
-                    continue
-            print(found)
-            if len(found) == 0:
-                results.append(expression)  # 결과를 찾을 수 없는 경우
-            elif len(found) == 1:
-                results.append(f"{left} = {found.pop()}")  # 유일한 결과가 있는 경우
+        if res != "X":
+            number += res
+        for i in express:
+            if i == "+" or i == "-":
+                calculate_stk.append(i)
             else:
-                results.append(f"{left} = ?")  # 결과가 여러 개인 경우
-        # results.append(expression)
+                number_stk.append(i)
+                number += i
+    number = sorted(number, key=lambda x: -int(x[0]))
+    start_base = int(number[0]) + 1
 
-    return results
+    # 뭐가 될지 찾아야함.
+    answer_list = []
+    for n in range(start_base, 10):
+        temp = 1
+        for correct in correct_expression:
+            x = valid_result(n, correct)
+            if not x:
+                temp = 0
+        if temp:
+            answer_list.append(n)
+
+    answer_list = list(set(answer_list))
+    answer = []
+    for incorrect_express in incorrect_expression:
+        sets = set()
+        for possible_base in answer_list:
+            sets.add(valid_result(possible_base, incorrect_express))
+        temp = str(sets.pop()) if len(sets) == 1 else '?'
+        answer.append(incorrect_express[:-1] + temp)
+    return answer
 
 
-print(solution(["51 - 5 = 44"]))
+print(solution(["14 + 3 = 17", "13 - 6 = X", "51 - 5 = 44"]))
 # print(solution(["1 + 1 = 2", "1 + 3 = 4", "1 + 5 = X", "1 + 2 = X"]))
 # print(solution(["10 - 2 = X", "30 + 31 = 101", "3 + 3 = X", "33 + 33 = X"]))
 # print(solution(["2 - 1 = 1", "2 + 2 = X", "7 + 4 = X", "5 - 5 = X"]))
