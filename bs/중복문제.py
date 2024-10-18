0d24f1c821a76ce27ef4d9273a8ecee9db74820c
boseok <fa7271@naver.com>
2024-10-18 19:30:34 +0900

    241018

--- bs/중복문제.py
+++ bs/중복문제.py
@@ -4,8 +4,20 @@ import re
 
 sys.stdin = open('/Users/song/Desktop/Python/Python/h.txt', 'r')
 
-x = list(input())
-dic = {chr(i) : 0 for i in range(97, 123)}
-for i in x:
-    dic[i] += 1
-print(*list(dic.values()))
+a, b = map(int, input().split())
+
+m = int(input())
+
+arr = list(map(int, input().split()))
+arr.reverse()
+ten = 0
+for i in range(m):
+    ten += arr[i] * (a ** i)
+
+result = []
+while ten // b:
+    result.append(ten % b)
+    ten = ten // b
+result.append(ten)
+result.reverse()
+print(' '.join(map(str, result)))
